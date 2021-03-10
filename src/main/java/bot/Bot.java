cfd65ecb6f1f05a27539cd374c8565a58f40cbed
Ravel Andrade <ravel.gou@gmail.com>
2021-03-10 15:16:03 -0300

    revisão de código

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -5,32 +5,41 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class Bot extends TelegramLongPollingBot{
+public class Bot extends TelegramLongPollingBot {
 
 	@Override
 	public void onUpdateReceived(Update update) {
-	    // We check if the update has a message and the message has text
-	    if (update.hasMessage() && update.getMessage().hasText()) {
-	        SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-	                .setChatId(update.getMessage().getChatId())
-	                .setText(update.getMessage().getText());
-	        try {
-	            execute(message); // Call method to send the message
-	        } catch (TelegramApiException e) {
-	            e.printStackTrace();
-	        }
-	    }
+		
+		if (update.hasMessage() && update.getMessage().hasText()) {
+			SendMessage message = createSendMessageWithMandatoryFields(update);
+			send(message);
+			
+		}
+	}
+
+	public SendMessage createSendMessageWithMandatoryFields(Update update) {
+		SendMessage message = new SendMessage().setChatId(
+				update.getMessage().getChatId()).setText(update.getMessage().getText());
+		return message;
+	}
+	
+	public void send(SendMessage message) {
+		try {
+			execute(message);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public String getBotUsername() {
-		
+
 		return null;
 	}
 
 	@Override
 	public String getBotToken() {
-		
+
 		return "1674642041:AAFTMDdMoKUSiCaWrOUpdh5V5ZKDcdM4Od0";
 	}
 
--- src/main/java/com/ravel/telegrambot/TelegrambotTask1Application.java
+++ src/main/java/com/ravel/telegrambot/TelegrambotTask1Application.java
@@ -1,4 +1,5 @@
 package com.ravel.telegrambot;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -10,23 +11,25 @@ import bot.Bot;
 public class TelegrambotTask1Application {
 
 	public static void main(String[] args) {
-		
-		 try {
-	            ApiContextInitializer.init();
-	            TelegramBotsApi telegramBotsApi = createLongPollingTelegramBotsApi();
-	            try {
-	                telegramBotsApi.registerBot(new Bot());
-	            } catch (TelegramApiException e) {
-	                
-	            }
-	        } catch (Exception e) {
-	           
-	        }
-		
+		initializeBot();
 	}
 	
+	private static void initializeBot() {
+		ApiContextInitializer.init();
+		TelegramBotsApi telegramBotsApi = createLongPollingTelegramBotsApi();
+		registerBot(telegramBotsApi);
+	}
+
+	private static void registerBot(TelegramBotsApi telegramBotsApi) {
+		try {
+			telegramBotsApi.registerBot(new Bot());
+		} catch (TelegramApiException e) {
+
+		}
+	}
+
 	private static TelegramBotsApi createLongPollingTelegramBotsApi() {
-        return new TelegramBotsApi();
-    }
-	
+		return new TelegramBotsApi();
+	}
+
 }
